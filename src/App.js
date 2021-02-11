570f20391bd96885fae0cc1ea49544b988b8f19f
unknown <n0laja00@students.oamk.fi>
2021-02-11 15:42:49 +0200

    Olen täysi idiootti. Korjaus.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import {useState, useEffect} from 'react';
-import Weather from './Weather';
+
 import Geolocation from './Geolocation';
 
 
@@ -9,52 +9,24 @@ const API_URL ='https://api.openweathermap.org/data/2.5/weather?';
 const ICON_URL = 'http://openweathermap.org/img/wn/'
 const API_KEY = '';
 
-//Kysymys opettajalle: Miten nämä componentit oltaisiin saatu kommunikoimaan toistensa kanssa? Minä halusin aluksi saada geolocationista nuo Latit ja Lng:t, mutta en vaan saanut millään toimimaan, joten tein näin.
+
 function App() {
 
-  const [lat, setLat] = useState(0); 
-  const [lng, setLng] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  
-
-  useEffect (() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-          setLat(position.coords.latitude);
-          setLng(position.coords.longitude);
-          setIsLoading(false);
-      }, (error) => {
-        alert(error);
-      })
-    } else {
-      alert('Your Browser does not support geolocation!')
-    }
-  }, [])
-
-
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-  else {
     return (
       <div className="container">
         <div className="row justify-content-center text-center">
           <div className="col-5">
               <div>
-                <Geolocation lat={lat} lng={lng}/>
-              </div> 
-              <div> 
-                <Weather lat={lat} lng={lng}/>     
+                <Geolocation/>
               </div> 
           </div>   
         </div>
       </div>
-)
+    )
   }
 
 
-}
+
 
 
 export default App;
--- src/Geolocation.js
+++ src/Geolocation.js
@@ -1,8 +1,33 @@
 import React from 'react'
 import {useState, useEffect} from 'react';
+import Weather from './Weather';
+export default function Geolocation() {
 
-export default function Geolocation({lat,lng}) {
+  const [lat, setLat] = useState(0); 
+  const [lng, setLng] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  
 
+  useEffect (() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+          setIsLoading(false);
+      }, (error) => {
+        alert(error);
+      })
+    } else {
+      alert('Your Browser does not support geolocation!')
+    }
+  }, [])
+
+
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  else {
     
     return (
         <div className="geolocation-container">
@@ -13,6 +38,10 @@ export default function Geolocation({lat,lng}) {
               <div>
                 <p>Lng: {lng.toFixed(3)} </p>
               </div> 
+              <div> 
+                <Weather lat={lat} lng={lng}/>     
+              </div> 
         </div>
     )
+  }
 }
